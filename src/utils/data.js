eb86a9dcb371bf89efde28bc6ad687ea1e7d76c8
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2021-07-04 09:49:10 -0300

    refactor(utils): add hash module

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,5 @@
 import config from 'config';
+import hash from './hash';
 import logger from './logger';
 
 const BUILD = process.env.REACT_APP_BBB_PLAYBACK_BUILD;
@@ -76,22 +77,6 @@ const buildFileURL = (recordId, file) => {
   if (mediaPath) fileUrl = `${mediaPath}/${fileUrl}`;
 
   return `${rootUrl}/${fileUrl}`;
-};
-
-// @ironwallaby's string-hash
-// https://github.com/darkskyapp/string-hash
-const hash = (str) => {
-  let hash = 5381;
-
-  let i = str.length;
-  while (i) {
-    hash = (hash * 33) ^ str.charCodeAt(--i);
-  }
-
-  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
-   * integers. Since we want the results to be always positive, convert the
-   * signed int to an unsigned by doing an unsigned bitshift. */
-  return hash >>> 0;
 };
 
 const getAvatarStyle = name => {
