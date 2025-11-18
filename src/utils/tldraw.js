6060f9045d79ccc7eef6af5f8fd5d2b126ef3f6f
germanocaumo <germanocaumo@gmail.com>
2025-11-18 14:48:05 -0300

    fix(tldraw): use correct tldraw version when there is no shapes
    Since BBB 3.0, when there is no shapes, there was a bug causing it to use the old tldraw version (v1), leaving the cursor and pan/zoom out of place.
    This fixes it by setting the version when there is no shapes and also by moving the cursor logic.

--- src/utils/tldraw.js
+++ src/utils/tldraw.js
@@ -7,7 +7,12 @@ import storage from 'utils/data/storage';
  * @returns {(string|undefined)} The BBB version associated with the Tldraw instance at the specified index. 
  *                               Returns undefined if no instance is found at the index.
  */
-const getTldrawBbbVersion = (index) => storage.tldraw[index]?.bbb_version;
+const getTldrawBbbVersion = (index) => {
+  if (index === -1) {
+    return storage.tldraw[0]?.bbb_version;
+  }
+  return storage.tldraw[index]?.bbb_version;
+};
 
 /**
  * Retrieves Tldraw data for a given slide index and page number.
@@ -98,7 +103,7 @@ const createTldrawImageAsset = (assetId, imageUrl, scaledWidth, scaledHeight) =>
 const createTldrawBackgroundShape = (assetId, curPageId, scaledWidth, scaledHeight) => {
   return {
     x: 1,
-    y: 1, 
+    y: 1,
     rotation: 0,
     isLocked: true,
     opacity: 1,
