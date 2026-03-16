07bf96dae3101ce2f31fcc332d5351d9f27b8f5e
Ramón Souza <contato@ramonsouza.com>
2026-03-16 15:48:09 -0300

    fix grouping of similar poll answers breaking playback

--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -68,6 +68,7 @@ const getBar = (percentage) => {
 };
 
 const getPads = (n) => {
+  if (n <= 0) return '';
   if (deviceInfo.osName === "Linux") {
     // Note the conditional branch below could be the other way around
     //  i.e. FIGURE_SPACE for Firefox and EN_SPACE for Chrome
